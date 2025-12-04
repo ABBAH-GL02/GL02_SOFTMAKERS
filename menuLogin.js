767ca586f38717c7770d2255b27885af06a65c65
Lilian Bernet <lilian.bernet@orange.fr>
2025-12-04 20:06:06 +0100

    Ajout boucle while

--- menuLogin.js
+++ menuLogin.js
@@ -14,49 +14,51 @@ export default async function menuLogin(rl = null) {
     }
 
     try {
-        console.log("\n---------------------------------------------------\n");
-        console.log("LOGIN");
-        console.log("1) Se connecter");
-        console.log("2) Créer un compte");
-        console.log("0) Quitter");
-        const choice = await rl.question("\nChoix > ");
+        while (true) {
+            console.log("\n---------------------------------------------------\n");
+            console.log("LOGIN");
+            console.log("1) Se connecter");
+            console.log("2) Créer un compte");
+            console.log("0) Quitter");
+            const choice = await rl.question("\nChoix > ");
 
-        if (choice.toLowerCase() === '1') {
-            const id = await rl.question("Identifiant : ");
-            rl.pause();
-            const question = [
-                {
-                    type: "password",
-                    name: "password",
-                    message: "Mot de passe : ",
-                    mask: '*',
+            if (choice === '1') {
+                const id = await rl.question("Identifiant : ");
+                rl.pause();
+                const question = [
+                    {
+                        type: "password",
+                        name: "password",
+                        message: "Mot de passe : ",
+                        mask: '*',
+                    }
+                ];
+
+                const password = await inquirer.prompt(question);
+                rl.resume();
+                const data = await fs.readFile(path.join(process.cwd(), "actions", "account.json"), "utf-8");
+                const enseignants = JSON.parse(data);
+
+                const user = enseignants.find(e => e.id === id && e.password === password.password);
+
+
+                if (user) {
+                    console.log(`\nConnexion réussie ! Bienvenue ${user.prenom} ${user.nom}.`);
+                    return user.role;
+                } else {
+                    console.log("\nIdentifiant ou mot de passe incorrect.");
+                    return menuLogin(rl);
                 }
-            ];
-
-            const password = await inquirer.prompt(question);
-            rl.resume();
-            const data = await fs.readFile(path.join(process.cwd(), "actions", "account.json"), "utf-8");
-            const enseignants = JSON.parse(data);
-
-            const user = enseignants.find(e => e.id === id && e.password === password.password);
-
-
-            if (user) {
-                console.log(`\nConnexion réussie ! Bienvenue ${user.prenom} ${user.nom}.`);
-                return user.role;
-            } else {
-                console.log("\nIdentifiant ou mot de passe incorrect.");
+            }
+            else if (choice === '2') {
+                await ajoutAccount(rl);
                 return menuLogin(rl);
             }
-        }
-        else if (choice.toLowerCase() === '2') {
-            await ajoutAccount(rl);
-            return menuLogin(rl);
-        }
-        else if (choice.toLowerCase() === '0') {
-            console.log("Vous avez quitté le logiciel.");
-            rl.close();
-            process.exit(0);
+            else if (choice === '0') {
+                console.log("Vous avez quitté le logiciel.");
+                rl.close();
+                process.exit(0);
+            }
         }
     } catch (err) {
         console.error("Erreur lors de la connexion :", err);
